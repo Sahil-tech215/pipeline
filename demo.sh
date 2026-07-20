f7afd2af963ad4e6b357475b6a9aae02f1def396
Sahil-tech215 <sahil@unicloud.co>
2026-07-20 22:30:03 +0530

    Add files via upload

---
+++ demo.sh
@@ -0,0 +1,21 @@
+ Volume in drive D is Data-Drive
+ Volume Serial Number is AE50-80E0
+
+ Directory of D:\Git-502
+
+05-10-2025  05:13 PM    <DIR>          .
+05-10-2025  11:27 PM    <DIR>          cicd-private-gke
+05-10-2025  11:27 PM               384 cicd-private-gke.bat
+05-10-2025  11:29 PM    <DIR>          cloudrun-gke-bridge
+05-10-2025  11:27 PM               390 cloudrun-gke-bridge.bat
+08-10-2025  10:01 PM    <DIR>          Infra-CLI-Guide
+05-10-2025  11:28 PM               382 Infra-CLI-Guide.bat
+03-11-2025  07:54 AM    <DIR>          Pipeline-For_GCP_VM
+05-10-2025  11:28 PM               390 Pipeline-For_GCP_VM.bat
+08-10-2025  10:01 PM    <DIR>          Pipeline-For_Public_GKE_Cluster
+05-10-2025  11:28 PM               414 Pipeline-For_Public_GKE_Cluster.bat
+05-10-2025  05:22 PM    <DIR>          portfolio
+31-10-2025  12:41 AM    <DIR>          pvt-repo
+05-10-2025  11:28 PM               368 pvt-repo.bat
+               6 File(s)          2,328 bytes
+               8 Dir(s)  118,973,423,616 bytes free
